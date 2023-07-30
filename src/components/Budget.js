df0af6a4e4b921003eee41c8fd52f0ce6ce57a66
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-07-29 22:54:04 -0700

    initial budjget.js code

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,13 @@
+import React, { useContext } fom 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const {budget} = userContext(AppContext);
+    return (
+        <div className = 'alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+        </div>
+    );
+};
+
+export default Budget;
