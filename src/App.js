f2343716fb754510d4c6ae538ddc2d160605cd3b
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-07-30 16:23:03 -0700

    update remaining.js code and add remaining to app file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -21,7 +21,9 @@ const App = () => {
                     </div>
 
                         {/* Add Remaining component here under */}        
-
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
                         {/* Add ExpenseTotal component here under */}        
                        
                         {/* Add ExpenseList component here under */}         
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -3,15 +3,15 @@ import { AppContext } from '../context/AppContext';
 
 const Remaining = () => {
     const { expenses, budget }= useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item)) => {
+    const totalExpenses = expenses.reduce((total, item) => {
         return (total = total + item.cost);
     }, 0);
     const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
-    return {
+    return (
         <div className={`alert ${alertType}`}>
             <span>Remaining: £{budget - totalExpenses}</span>
         </div>
-    };
+    );
 };
 
 export default Remaining;
